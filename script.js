e16b545d1954918f26092afe3ded6ab89c0f0019
Sarah Cavanaugh <sarahcavanaugh20@gmail.com>
2024-01-25 18:43:42 -0500

    timer app update

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 var timerBtn = document.getElementById('timerBtn');
 var timerInput = document.getElementById('timer');
 var timeAmount = timerInput.value;
-var timeRemaining = 50;
+var containerDiv = document.getElementById('newDiv');
 
 function displayTime() {
 	var div = document.createElement('div');
 	document.body.innerHTML = ''; // clear line each time
 	document.body.appendChild(div); //putting on the page
 	div.innerHTML = timerInput.value; //the text (innerHTML) is showing the time remaining on the screen
-	console.log(timerInput);
+
 	console.log(timerInput.value);
 }
-
+function displayPic() {
+	surprisePic = document.getElementById('myPic').display('block');
+	containerDiv.appendChild(surprisePic);
+}
 function startCountdown() {
 	setInterval(function () {
 		timerInput.value--; //subtract 1 each second
 		displayTime(); //showing remaining time after subtractions
 		if (timerInput.value === 0) {
-			clearInterval(timer); // stop counting down (stops at 0)
+			displayPic(); // stop counting down (stops at 0)
 		}
 	}, 1000); // countdown by 1 millisecond intervals
 }
 
 timerBtn.addEventListener('click', function (event) {
-	event.preventDefault();
 	startCountdown();
 });
